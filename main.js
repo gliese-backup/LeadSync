2f1a772558201ba5bf32b78a69176d5ecc7148ef
gliese-backup <glieseb+1@proton.me>
2025-01-22 10:52:10 +0000

    add reload

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ const yargs = require("yargs/yargs"); // cli se read karne deta hai
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
 
-console.log(argv.output);
+console.log(argv.output, argv.input);
+
+console.log("Hello");
